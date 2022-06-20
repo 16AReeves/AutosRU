479aaaa8993b046ad0122af70a8c3b996fb9aff9
Abigayle Reeves <16areeves@gmail.com>
2022-06-20 14:59:47 -0400

    pushing challenge code

--- R_Analysis/MechaCarChallenge.R
+++ R_Analysis/MechaCarChallenge.R
@@ -1,3 +1,5 @@
+#Deliverable 1:
+
 # import dependencies
 library(tidyverse)
 
@@ -9,3 +11,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # summarize the linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car))
+
+
+# Deliverable 2:
+
+# read in csv file
+car_coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+# summarize PSI Column
+total_summary <- car_coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_PSI=sd(PSI), .groups= "KEEP")
+
+# summarize data by manufacturing lot
+lot_summary <- car_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_PSI=sd(PSI), .groups= "keep")
